f618cdf8ff1ff6dd641ddaaa77a0a5eeebc18050
lincong <lincong8722@qq.com>
2025-09-22 15:54:01 +0800

    DEBUG:fix __init__

--- voxelmorph/__init__.py
+++ voxelmorph/__init__.py
@@ -13,23 +13,52 @@ from packaging import version
 import neurite
 minv = '0.2'
 curv = getattr(neurite, '__version__', None)
-
 if curv is None or version.parse(curv) < version.parse(minv):
     raise ImportError(f'voxelmorph requires neurite version {minv} or greater, '
                       f'but found version {curv}')
 
-import tensorflow
-# ensure valid tensorflow version is available
-minv = '2.4'
-curv = getattr(tensorflow, '__version__', None)
-if curv is None or version.parse(curv) < version.parse(minv):
-    raise ImportError(f'voxelmorph requires tensorflow version {minv} or greater, '
-                        f'but found version {curv}')
-
-
-from . import py
-from . import utils
+# move on the actual voxelmorph imports
 from . import generators
-from . import layers
-from . import losses
-from . import networks
+from . import py
+from .py.utils import default_unet_features
+
+
+# import backend-dependent submodules
+backend = py.utils.get_backend()
+
+if backend == 'pytorch':
+    # the pytorch backend can be enabled by setting the VXM_BACKEND
+    # environment var to "pytorch"
+    try:
+        import torch
+    except ImportError:
+        raise ImportError('Please install pytorch to use this voxelmorph backend')
+
+    os.environ['NEURITE_BACKEND'] = 'pytorch'
+
+    from . import torch
+    from .torch import layers
+    from .torch import networks
+    from .torch import losses
+
+else:
+    # tensorflow is default backend
+    try:
+        import tensorflow
+    except ImportError:
+        raise ImportError('Please install tensorflow to use this voxelmorph backend')
+
+    os.environ['NEURITE_BACKEND'] = 'tensorflow'
+
+    # ensure valid tensorflow version is available
+    minv = '2.4'
+    curv = getattr(tensorflow, '__version__', None)
+    if curv is None or version.parse(curv) < version.parse(minv):
+        raise ImportError(f'voxelmorph requires tensorflow version {minv} or greater, '
+                          f'but found version {curv}')
+
+    from . import tf
+    from .tf import layers
+    from .tf import networks
+    from .tf import losses
+    from .tf import utils
